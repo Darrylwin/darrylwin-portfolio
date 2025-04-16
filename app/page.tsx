c0209f0e1762be54490a0d4aa2399b45779dece4
Darrylwin <logossoudarryl20@gmail.com>
2025-04-16 08:52:51 +0000

    feat: add Spacing component and integrate it into Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import {Header} from "@/app/_components/Header";
 import {Hero} from "@/app/_components/Hero";
+import {Spacing} from "@/app/_components/Spacing";
 
 export default function Home() {
     return <main>
         <Header/>
+        <Spacing size="md"/>
         <Hero></Hero>
     </main>
 }
