445743c34b43371ccd5cb93903d76f811be652d6
Darrylwin <logossoudarryl20@gmail.com>
2025-04-16 08:52:27 +0000

    feat: add FlutterIcon component and update globals.css with new text-primary variable

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const AnekTelugu = Anek_Telugu({
 //     variable: "--font-geist-sans",
 //     subsets: ["latin"],
 // });
-//
+
 // const geistMono = Geist_Mono({
 //     variable: "--font-geist-mono",
 //     subsets: ["latin"],
@@ -32,6 +32,7 @@ export default function RootLayout({children}: Readonly<{ children: React.ReactN
         <html lang="en" className='h-full'>
         <body className={cn(
             GeistSans.variable,
+            // GeistMono.variable,
             AnekTelugu.variable,
             "font-sans h-full bg-background text-foreground"
         )}>
