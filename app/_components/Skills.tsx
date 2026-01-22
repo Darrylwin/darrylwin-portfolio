6c3fad8a48e23d6d576eaa0f089ca69824490c76
Darrylwin <logossoudarryl20@gmail.com>
2026-01-22 19:08:18 +0000

    updated skills descriptions

--- app/_components/Skills.tsx
+++ app/_components/Skills.tsx
@@ -17,9 +17,7 @@ export const Skills = () => {
           <FlutterIcon size={42} />
           <h3 className="text-2xl font-semibold tracking-tight">Flutter</h3>
           <p className="text-muted-foreground text-sm">
-            My main framework is <Code>Flutter</Code> for mobile development. I also use{" "}
-            <Code>Next.Js</Code> as a frontend framework for web interfaces and,
-            <Code>Nest.Js</Code> and <Code>Laravel</Code> as backend frameworks.
+            My <u>main</u> framework is <Code>Flutter</Code> for <u>mobile</u> development.
           </p>
         </div>
 
@@ -28,7 +26,7 @@ export const Skills = () => {
           <h3 className="text-2xl font-semibold tracking-tight">Next.Js</h3>
           <p className="text-muted-foreground text-sm">
             I can create <u>beautiful</u> and <u>performant</u> web interfaces
-            using <Code>Next.Js</Code>
+            using <Code>Next.Js</Code>.
           </p>
         </div>
         
@@ -39,7 +37,7 @@ export const Skills = () => {
           </div>
           <h3 className="text-2xl font-semibold tracking-tight">Nest.Js and Laravel</h3>
           <p className="text-muted-foreground text-sm">
-            I can create <u>efficient</u>, <u>robust</u> and <u>scalable</u> backend applications
+            I can create <u>efficient</u> and <u>scalable</u> backend applications
             using <Code>Nest.Js</Code> and <Code>Laravel</Code>.
           </p>
         </div>
@@ -48,7 +46,7 @@ export const Skills = () => {
           <ClaudeIcon size={42} style={{ animationDuration: "10s" }} />
           <h3 className="text-2xl font-semibold tracking-tight">Claude</h3>
           <p className="text-muted-foreground text-sm">
-            I&apos;m good at integrating AI features in your applications to
+            I&apos;m good at integrating <u>AI</u> features in your applications to
             create a perfect <u>user experience</u>.
           </p>
         </div>
